fc92f828738c9a072aedbc71cd581f4213de9c98
maymarquee <231035731@aluno.unb.br>
2023-11-13 01:38:36 -0300

    Mudança na fonte Polegar Verde

--- App.jsx
+++ App.jsx
@@ -4,13 +4,13 @@ import { useState } from 'react';
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import InterRegular from './assets/fonts/Inter-Regular.ttf';
-import SyneRegular from './assets/fonts/Syne-Regular.ttf';
+import SyneBold from './assets/fonts/Syne-Bold.ttf';
 
 export default function App() {
     const [renderizacao, setRenderizacao] = useState(false)
     Font.loadAsync({
       'Inter': InterRegular, 
-      'Syne': SyneRegular,
+      'Syne': SyneBold,
     }).then(()=>{
       setRenderizacao(true)
     })
