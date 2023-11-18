f30c009e620f329c22fcae9da235df83d6c265d8
maymarquee <231035731@aluno.unb.br>
2023-11-17 21:29:28 -0300

    Alteração no botão: onpress, cor texto, children

--- assets/components/Button.jsx
+++ assets/components/Button.jsx
@@ -1,32 +1,27 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { useState } from 'react'
+import { StyleSheet, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
 
 export default function Button(props){
-    const [color, setColor] = useState(props.cor);
 
-    const coloridinho = () => {
-        const newColoridinho = {cor}
-        setColor = newColoridinho
-    }
-
-  return (
-    <TouchableOpacity styles={styles.button}
-        onPress={props.paralelepipedo}
-    >
-        <Text style={{ color }}>
-        {props.labelButton}
+    return (
+    <TouchableOpacity onPress={props.onPress} style={{
+        ...props.style,
+        ...styles.button
+    }}>
+        <Text style={{
+            color: props.style.color ? props.style.color : "#ffffff"
+        }}>
+            {props.children}
         </Text>
     </TouchableOpacity>
-  )
+    )
 }
 
 const styles = StyleSheet.create({
     button: {
-        height: 42,
-        width: 312,
+        display: "flex",
         justifyContent: 'center',
         alignItems: 'center',
         borderRadius: 8,
     },
-    
 })
